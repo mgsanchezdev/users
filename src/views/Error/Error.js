d4f5e1ca0f6336d9dcce97637bbefeb4775a699b
Mario Sanchez <mgsanchezdev@gmail.com>
2021-03-07 17:19:01 -0300

    Realice la logica de la vista Error

--- src/views/Error/Error.js
+++ src/views/Error/Error.js
@@ -1,5 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Error.css';
 
-const Error = () => <div />;
-
+const Error = () => (
+  <>
+    <div className="error">
+      <h1>404</h1>
+      <p>No encontramos lo que buscabas.</p>
+      <Link className="buttonx" to="/">
+        <i className="icon-home" /> Haz click aca para regresar!
+      </Link>
+    </div>
+  </>
+);
 export default Error;
